f3c4045d305845928c939797c68497b933063a1f
Ganesh Mane <ganeshamane800@gmail.com>
2025-01-20 01:49:22 +0530

    Add files via upload

---
+++ src/context/notes/noteContext.js
@@ -0,0 +1,8 @@
+// Purpose-created context API.
+
+// Imported the create createContext from react to create the context api
+import { createContext } from "react";
+
+const noteContext = createContext(); // Created a note context api
+
+export default noteContext;
