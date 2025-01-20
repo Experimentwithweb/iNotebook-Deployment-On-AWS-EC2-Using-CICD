435827af8ae47bdc67308a93e272a03988b17863
Ganesh Mane <ganeshmane714@gmail.com>
2025-01-20 13:26:02 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
 
   return (  //render method of about component
     <div>
-     <h1> iNotebook app stores your notes in the cloud securely second commit new vm </h1>
+     <h1> iNotebook app stores your notes in the cloud securely second commit new vm third commit </h1>
     </div>
   )
 }
